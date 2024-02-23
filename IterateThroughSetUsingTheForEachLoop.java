a090754a3480fb9ffe11f01b9bd5f643bfaa8c77
Vishal Gavasekar <93640334+vishalgavasekar@users.noreply.github.com>
2024-02-23 11:21:16 +0530

    from 167 question

---
+++ IterateThroughSetUsingTheForEachLoop.java
@@ -0,0 +1,20 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Main {
+    public static void main(String[] args) {
+        // Creating an set
+        Set<String> languages = new HashSet<>();
+        languages.add("Java");
+        languages.add("JavaScript");
+        languages.add("Python");
+        System.out.println("Set: " + languages);
+
+        // Using forEach loop
+        System.out.println("Iterating over Set using for-each loop:");
+        for (String language : languages) {
+            System.out.print(language);
+            System.out.print(", ");
+        }
+    }
+}
